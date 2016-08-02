6b3b6b5bdb804c076db8c547a72f8d01be1c4c40
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2016-08-02 17:54:53 +0530

    Removed extra breakpoints.

--- v1/labs/graphs/__init__.py
+++ v1/labs/graphs/__init__.py
@@ -13,10 +13,8 @@ sql_engine = create_engine('postgresql://ubuntu:electric123@data.engazeapp.com:5
 
 @blueprint.route('/labs/graphs/consumptionChange/morning')
 def morning_compare():
-	print "Inside route handler"
 	data_to_send = morning_change(sql_engine)
 	print data_to_send
-	print "After call, in handler"
 	return jsonify(data_to_send)
 
 @blueprint.route('/labs/graphs/messages/daywise')
--- v1/labs/graphs/consumption_change_morning.py
+++ v1/labs/graphs/consumption_change_morning.py
@@ -32,17 +32,15 @@ def return_hour_consumption_before(row, kwh_df, hour):
     hour_consumption_before = kwh_df[(kwh_df.inst == user_inst) & (kwh_df.date.dt.date != date) & (kwh_df.date.dt.hour == hour) & (kwh_df.date.dt.weekday == weekday)].kwh.mean()
     return hour_consumption_before
 
-    
+
 def morning_change(sqlengine):
     #### DATE AND TIME SETTINGS #################
-    print "Inside function"
     test_start_day = datetime.date(2016, 7, 2)
     test_end_day = datetime.date.today() - datetime.timedelta(days = 4)
     start_dt = test_start_day - datetime.timedelta(days=42)
     start_dt = datetime.datetime.combine(start_dt, datetime.time(0, 0, 0))
     end_dt = test_end_day
     end_dt = datetime.datetime.combine(end_dt, datetime.time(23, 0, 0))
-    print "before messages data"
     #### MESSAGES DATA ##########################
     get_message_url = "http://beta.engazeapp.com/api/get/all/message/{0}%2000:00:00/{1}%2000:00:00".format(test_start_day, test_end_day)
     response = urllib2.urlopen(get_message_url)
@@ -56,7 +54,6 @@ def morning_change(sqlengine):
     messages_df.user_id = messages_df.user_id.astype(int)
     messages_df = messages_df[messages_df.message_reply == 1]
     messages_df = messages_df[messages_df.pledge_type_id == 1]
-    print "before user instance number"
     #### USER INSTANCE NUMBER ###################
     get_inst_url = "http://beta.engazeapp.com/api/get/all/installation/number"
     response = urllib2.urlopen(get_inst_url)
@@ -67,30 +64,24 @@ def morning_change(sqlengine):
     user_inst_df = user_inst_df[pandas.notnull(user_inst_df.user_id)]
     user_inst_df.user_id = user_inst_df.user_id.astype(int)
     user_inst_df.user_inst = user_inst_df.user_inst.astype(int)
-    print "merge with inst number"
     #### MERGE WITH INST NUMBER #################
     messages_df.user_id = messages_df.user_id.astype(int)
     messages_df = messages_df.merge(user_inst_df, on = ['user_id'])
     #### CONSUMPTION DATA #######################
     inst_list = list(set(messages_df.user_inst))
     inst_list_query = str(tuple(inst_list)).rstrip(',)') + ')'
-    print "before query"
     query = 'SELECT * FROM data WHERE inst IN {0} AND date BETWEEN \'{1}\' AND \'{2}\';'.format(inst_list_query, start_dt, end_dt)
     kwh_df = pandas.read_sql_query(query,con=sqlengine)
     #### FIND HOUR WISE CONSUMPTION #############
-    print "after query"
     hourly_consumption_before = ['before']
     hourly_consumption_after = ['after']
-    print "break 1"
     for hour in range(5, 12):
         print "break in loop"
-        hour_consumption_before_value = messages_df.apply(return_hour_consumption_before, axis = 1, kwh_df = kwh_df, hour = hour).sum()
-        hourly_consumption_before.append(round(hour_consumption_before_value,1))
-        hour_consumption_after_value = messages_df.apply(return_hour_consumption_after, axis = 1, kwh_df = kwh_df, hour = hour).sum()
-        hourly_consumption_after.append(round(hour_consumption_after_value,1))
+        # hour_consumption_before_value = messages_df.apply(return_hour_consumption_before, axis = 1, kwh_df = kwh_df, hour = hour).sum()
+        # hourly_consumption_before.append(round(hour_consumption_before_value,1))
+        # hour_consumption_after_value = messages_df.apply(return_hour_consumption_after, axis = 1, kwh_df = kwh_df, hour = hour).sum()
+        # hourly_consumption_after.append(round(hour_consumption_after_value,1))
         print "end of loop"
-    print "break 2"
     c3_data = { 'columns': [hourly_consumption_before, hourly_consumption_after] }
-    print "Before return"
     return c3_data
 
