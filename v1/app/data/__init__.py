4ab83452100632225219dd0922d7ab5e48c70953
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2016-08-02 18:39:45 +0530

    Corrected function argument var names for backup/failover handler functions.

--- v1/app/data/__init__.py
+++ v1/app/data/__init__.py
@@ -30,8 +30,8 @@ def suggested_activities(lang, instance_no, goals_csv):
 @blueprint.route('/activities/<string:lang>/<int:instance_num>/<string:goal_csv>')
 def suggested_activities2(lang, instance_num, goal_csv):
 	## ADD ERROR HANDLING HERE ##
-    if goals_csv is None:
-        goals_csv = "1,1,1"
+    if goal_csv is None:
+        goal_csv = "1,1,1"
     ## ADD ERROR HANDLING HERE ##
     goal = goals_csv.split(",")
     activities  = list_suggested_activities(goal, instance_no, lang, sql_engine)
